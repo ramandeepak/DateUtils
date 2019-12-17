71066100d212887f728fb4c8d54370dfa70fee00
Deepak Raman <deepak.raman@lighthousesoftware.com>
2019-12-17 01:40:41 -0600

    Adding enums of various date formats.

---
+++ DateUtils/DateUtils/DUDateFormat.swift
@@ -0,0 +1,22 @@
+//
+//  DUDateFormat.swift
+//  DateUtils
+//
+//  Created by Deepak Raman on 12/17/19.
+//  Copyright © 2019 Deepak Raman. All rights reserved.
+//
+
+import Foundation
+
+public enum DUDateFormat: String, CaseIterable {
+    
+       case standard = ""
+    
+       case short    = "123"
+    
+       case medium   = "1234"
+    
+       case long     = "12345"
+    
+       case full     = "123456"
+}
